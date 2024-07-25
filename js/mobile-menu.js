b983a4fb1121e8d6493bb22f763c2b7454fe71fc
Alexander Rostyagai <136843641+Rostyagai@users.noreply.github.com>
2024-07-25 13:00:36 +0300

    Close mobile menu when clicking link

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const dialog = document.getElementById("mobileOverlay");
-  const closeButton = dialog.querySelector(".mobile-overlay__close-button");
+  const mobileMenu = document.getElementById("mobileOverlay");
+  const mobileMenuLinks = document.querySelectorAll("#mobileOverlay a");
+  const closeButton = mobileMenu.querySelector(".mobile-overlay__close-button");
+
+  mobileMenuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      mobileMenu.classList.add("dialog-closing");
+      setTimeout(() => {
+        mobileMenu.classList.remove("dialog-closing");
+        mobileMenu.close();
+      }, 300);
+    });
+  });
 
   closeButton.addEventListener("click", (event) => {
     event.preventDefault();
-    dialog.classList.add("dialog-closing");
+    mobileMenu.classList.add("dialog-closing");
     setTimeout(() => {
-      dialog.classList.remove("dialog-closing");
-      dialog.close();
+      mobileMenu.classList.remove("dialog-closing");
+      mobileMenu.close();
     }, 300); // Duration of the fadeOut animation
   });
 });
